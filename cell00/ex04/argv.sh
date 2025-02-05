f4392ff7bf4250ce756862b3acee7669f72c5223
OakTinYamephan <tinyame2303@gmail.com>
2025-02-05 16:45:58 +0700

    new

--- cell00/ex04/argv.sh
+++ cell00/ex04/argv.sh
@@ -1,3 +1,8 @@
-echo "arguments 1 : $1"
-echo "arguments 2 : $2"
-echo "arguments 3 : $3"
+if [ $# -eq 0 ]; then
+    echo "No arguments supplied"
+else
+    # Loop through the first three arguments
+    for arg in "${@:1:3}"; do
+        echo "$arg"
+    done
+fi
